b1f6b55b546d8c1b2b9e508cacd328b0e153f77a
Miguel Delgado <migueldelgado@MacBook-Air-de-Miguel.local>
2021-02-26 05:03:38 -0600

    Project

---
+++ MiguelFramework/MiguelFramework.h
@@ -0,0 +1,18 @@
+//
+//  MiguelFramework.h
+//  MiguelFramework
+//
+//  Created by Miguel Delgado on 26/02/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MiguelFramework.
+FOUNDATION_EXPORT double MiguelFrameworkVersionNumber;
+
+//! Project version string for MiguelFramework.
+FOUNDATION_EXPORT const unsigned char MiguelFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MiguelFramework/PublicHeader.h>
+
+
---
+++ Pods/Target Support Files/Pods-MiguelFramework/Pods-MiguelFramework-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MiguelFramework : NSObject
+@end
+@implementation PodsDummy_Pods_MiguelFramework
+@end
